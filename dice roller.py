0e684fc8b364823bdc4f1cced99530974dd06337
xanatha <65658281+xanatha@users.noreply.github.com>
2020-05-23 23:12:48 +0100

    Add files via upload

---
+++ dice roller.py
@@ -0,0 +1,15 @@
+import random
+
+d4 = random.randint(1, 4)
+d6 = random.randint(1, 6)
+d8 = random.randint(1, 8)
+d10 = random.randint(1, 10)
+d12 = random.randint(1, 12)
+d20 = random.randint(1, 20)
+
+#print(d4)
+#print(d6)
+#print(d8)
+#print(d10)
+#print(d12)
+#print(d20)
